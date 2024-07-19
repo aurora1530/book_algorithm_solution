9ee4ddcee60e894f295fc7c26da6e6d81ace0b35
aurora1530 <35615953+aurora1530@users.noreply.github.com>
2024-07-19 17:50:21 +0900

    implement: graph type

---
+++ src/chapter13/lecture/type.ts
@@ -0,0 +1,13 @@
+export type Graph = number[][]
+
+export const sampleGraph: Graph = [
+  [1, 4, 2],
+  [0, 3, 4, 8],
+  [0, 5],
+  [1, 8, 7],
+  [0, 8],
+  [2, 6, 8],
+  [5, 7],
+  [3, 6],
+  [1, 3, 4, 5],
+];
